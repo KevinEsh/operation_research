241ea24622c5c937a48c0daeacedf93ca84f9dbf
KevinEsh <kevin_esh@outlook.com>
2025-07-26 13:09:09 -0600

    mlflow now works with minio and postgrslq

---
+++ postgres-init/init-dbs.sql
@@ -0,0 +1,6 @@
+SELECT 'CREATE DATABASE dbcore'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'dbcore')\gexec
+SELECT 'CREATE DATABASE dbmlflow'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'dbmlflow')\gexec
+SELECT 'CREATE DATABASE dbmetaflow'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'dbmetaflow')\gexec
